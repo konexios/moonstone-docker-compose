9fbd68b031c97cd9aeae3fc964dc210ea87bb6ef
Tam Nguyen <tam.nguyen1@arrow.com>
2019-07-12 23:11:10 +0000

    initial version

---
+++ bin/delete-data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo rm -rf ./var/lib/mongodb/*
+sudo rm -rf ./var/lib/rabbitmq/*
+sudo rm -rf ./var/lib/elasticsearch/*
+sudo rm -rf ./var/lib/zookeeper/data/*
+sudo rm -rf ./var/lib/zookeeper/datalog/*
+sudo rm -rf ./var/lib/kafka/*
+sudo rm -rf ./var/lib/redis/*
---
+++ bin/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ./var/lib/mongodb/*
+mkdir -p ./var/lib/rabbitmq/*
+mkdir -p ./var/lib/elasticsearch/*
+mkdir -p ./var/lib/zookeeper/data/*
+mkdir -p ./var/lib/zookeeper/datalog/*
+mkdir -p ./var/lib/kafka/*
+mkdir -p ./var/lib/redis/*
