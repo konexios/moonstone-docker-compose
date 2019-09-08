889bd06e386c095a1a42dc789e5eed5b082b408f
Tam Nguyen <tam.nguyen1@arrow.com>
2019-09-08 06:40:54 +0000

    update to version 19.2

--- bin/delete-data.sh
+++ bin/delete-data.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-sudo rm -rf ./var/lib/mongodb/*
-sudo rm -rf ./var/lib/rabbitmq/*
-sudo rm -rf ./var/lib/elasticsearch/*
-sudo rm -rf ./var/lib/zookeeper/data/*
-sudo rm -rf ./var/lib/zookeeper/datalog/*
-sudo rm -rf ./var/lib/kafka/*
-sudo rm -rf ./var/lib/redis/*
+set -x
+
+sudo docker ps | grep "moonstone" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+sudo docker ps -a | grep "moonstone" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+
+sudo docker ps | grep "mongodb" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+sudo docker ps -a | grep "mongodb" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+sudo docker volume ls | grep "mongodb" | awk '{print $2}' | xargs --no-run-if-empty sudo docker volume rm
+
+sudo docker ps | grep "elasticsearch" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+sudo docker ps -a | grep "elasticsearch" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+sudo docker volume ls | grep "elasticsearch" | awk '{print $2}' | xargs --no-run-if-empty sudo docker volume rm
+
+sudo docker ps | grep "kafka" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+sudo docker ps -a | grep "kafka" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+sudo docker volume ls | grep "kafka" | awk '{print $2}' | xargs --no-run-if-empty sudo docker volume rm
+
+sudo docker ps | grep "rabbitmq" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+sudo docker ps -a | grep "rabbitmq" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+sudo docker volume ls | grep "rabbitmq" | awk '{print $2}' | xargs --no-run-if-empty sudo docker volume rm
+
+sudo docker ps | grep "zookeeper" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+sudo docker ps -a | grep "zookeeper" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+sudo docker volume ls | grep "zookeeper" | awk '{print $2}' | xargs --no-run-if-empty sudo docker volume rm
+
+sudo docker ps | grep "redis" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+sudo docker ps -a | grep "redis" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+sudo docker volume ls | grep "redis" | awk '{print $2}' | xargs --no-run-if-empty sudo docker volume rm
--- bin/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./var/lib/mongodb/*
-mkdir -p ./var/lib/rabbitmq/*
-mkdir -p ./var/lib/elasticsearch/*
-mkdir -p ./var/lib/zookeeper/data/*
-mkdir -p ./var/lib/zookeeper/datalog/*
-mkdir -p ./var/lib/kafka/*
-mkdir -p ./var/lib/redis/*
